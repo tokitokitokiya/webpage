834fc5a6cf3c51d16b50b7e236ff0e51e7e67216
tokitokitokiy <tokitokitokiyahime@yahoo.co.jp>
2025-08-04 18:02:35 +0900

    目玉実装

--- kira.js
+++ kira.js
@@ -2,6 +2,7 @@
 /* 1. 変数の定義 */
 let bgImage;
 let buttons = [];
+let eyes = [];
 const buttonPositions = [
   { x: 0.1, y: 0.225 },
   { x: 0.5, y: 0.25 },
@@ -13,17 +14,18 @@ const NUM_POINTS = 80; //点の数
 const MAX_LINE_DIST = 120; //線の数
 const points = []; //点の座標と速度配列
 
+
 function preload() {
   // 画像を読み込み
   bgImage = loadImage('img/haikei.jpg');
 }
+
 
 function setup() {
   /* 2. キャンバスの初期化 */
   createCanvas(windowWidth, windowHeight); // 画面サイズ
   background(0);
   stroke(255, 80);
-
 
   /* 3. 点群の初期化 */
   for (let i = 0; i < NUM_POINTS; i++) {
@@ -35,6 +37,7 @@ function setup() {
     };
     points.push(pointxy);
   }
+
 
     // ボタンの生成
   for (let i = 0; i < buttonPositions.length; i++) {
@@ -59,11 +62,18 @@ function setup() {
 
     buttons.push(btn);
   }
+
+
+  //目玉の画像を挿入
+    const kurome = loadImage('img/alphatest.png');
+    eyes.push(new Eye(0.4, 0.2, 0.5, 0.225, width, height)); 
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight); // 高さは固定、横幅だけ画面に合わせる
 }
+
 
 function draw() {
   background(250);
@@ -129,4 +139,54 @@ function draw() {
   }
 
 
+  let target = createVector(mouseX, mouseY);
+  for (let eye of eyes) {
+    eye.show(target);
+    eye.wid=newWidth;
+    eye.heigh=newHeight;
+  }
+}
+
+
+
+class Eye {
+  constructor(a1, a2, x, y, wid, heigh) {
+    this.a1 = a1;
+    this.a2 = a2;
+    this.x = x;
+    this.y = y;
+    this.wid = wid;
+    this.heigh = heigh;
+
+    this.r1 = a1 * wid;
+    this.r2 = a2 * wid;
+    this.r3 = this.r2 * 0.5; // 黒目サイズ
+
+    this.origin = createVector(wid * x, heigh * y);
+    this.pos = this.origin.copy(); // 現在の瞳の位置
+  }
+
+  show(target) {
+    fill(360);
+    ellipse(this.origin.x, this.origin.y, this.r1, this.r1);
+
+    // 瞳の移動計算
+    let diff = p5.Vector.sub(target, this.origin);
+    let maxDist = (this.r1 - this.r2) / 2;
+    diff.setMag(constrain(diff.mag(), 0, maxDist));
+    let goal = p5.Vector.add(this.origin, diff);
+
+    let t = 0.1;
+    let easedT = this.easeInOut(t);
+    this.pos = p5.Vector.lerp(this.pos, goal, easedT);
+
+    fill(40, 100, 80);
+    ellipse(this.pos.x, this.pos.y, this.r2, this.r2);
+    fill(0);
+    ellipse(this.pos.x, this.pos.y, this.r3, this.r3);
+  }
+
+  easeInOut(t) {
+    return t * t * (3 - 2 * t);
+  }
 }

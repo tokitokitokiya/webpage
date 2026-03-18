d412b9d1d2908a5a91a917cc02aafffdb5864210
tokitokitokiy <tokitokitokiyahime@yahoo.co.jp>
2026-03-18 14:02:55 +0900

    ボタン消去

--- kira.js
+++ kira.js
@@ -8,13 +8,6 @@ let newWidth=800;
 let buttons = [];
 let eyes = [];
 let reSize = false;
-const buttonPositions = [
-  { x: 1/12, y: 1/6 },
-  { x: 0.5, y: 0.25 },
-  { x: 0.7, y: 0.6 },
-  { x: 0.3, y: 0.75 },
-  { x: 0.8, y: 0.4 }
-];
 const NUM_POINTS = 80; //点の数
 const MAX_LINE_DIST = 120; //線の数
 const points = []; //点の座標と速度配列
@@ -43,31 +36,6 @@ function setup() {
         vy: random(-1, 1),
     };
     points.push(pointxy);
-  }
-
-
-    // ボタンの生成
-  for (let i = 0; i < buttonPositions.length; i++) {
-    const btn = createButton('');
-    btn.style('position', 'absolute');
-    btn.style('z-index', '10');
-    btn.style('border', 'none');
-    btn.style('padding', '0');
-    btn.style('background', 'none');
-
-    // ボタンに画像を入れる
-    const img = createImg(`img/blockt.png`, `btn${i + 1}`);
-    img.parent(btn);
-    img.style('width', '100%');
-    img.style('height', '100%');
-    img.style('object-fit', 'cover');
-
-    // イベントも任意に追加
-    btn.mousePressed(() => {
-      console.log(`ボタン${i + 1} clicked`);
-    });
-
-    buttons.push(btn);
   }
 
   //目玉の画像を挿入
@@ -113,33 +81,6 @@ function draw() {
       }
     }
   }
-
-  // ボタン位置・サイズを更新
-  // 画像に対するn%の幅
-  const btnWs = [
-    newWidth * 0.05,
-    newWidth * 0.1,
-    newWidth * 0.1,
-    newWidth * 0.1,
-    newWidth * 0.1
-  ]; 
-  const btnHs = [
-    newHeight * 0.05,
-    newHeight * 0.1,
-    newHeight * 0.1,
-    newHeight * 0.1,
-    newHeight * 0.1
-  ];
-
-  for (let i = 0; i < buttons.length; i++) {
-    const pos = buttonPositions[i];
-    const x = newWidth * pos.x;
-    const y = newHeight * pos.y;
-
-    buttons[i].position(x, y);
-    buttons[i].size(btnWs[i], btnHs[i]);
-  }
-
 
   for (let eye of eyes) {
     eye.koushin(mouseX,mouseY);
